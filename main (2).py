f5a4e3eee1f2e981deeeba7db456e9e3ad86e4d1
Fariz Akbar <farizakbar1222@gmail.com>
2026-04-09 07:07:20 +0700

    Rename main (2).py to try2_oop.py

--- main (2).py
+++
@@ -1,2 +0,0 @@
-print("Haloo sayaa farizz");print("bisa dipanggil Rizz");print("mirip Khabib ");print("Ganteng bangets")
-print("Hobi main game");print("extrovert kadang juga introvert");print("cita cita jadi TNI");print("umur 16 tahun")
