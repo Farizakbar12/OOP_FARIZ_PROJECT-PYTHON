33ec54ec5b7535d0b02b301350086d40fcc5c73a
Fariz Akbar <farizakbar1222@gmail.com>
2026-04-30 09:11:18 +0700

    Add files via upload

---
+++ Try11_oop.py
@@ -0,0 +1,6 @@
+nilai = 74
+
+if nilai >=75:
+  print("lulus")
+else:
+    print("tidak lulus")
---
+++ Tugas5_oop.py
@@ -0,0 +1,11 @@
+suhu=int(input("masukkan suhu farenhait:"))
+
+Celcius=(suhu-32)*5/9
+ 
+if Celcius > 30:
+  print("panas")
+   
+else:
+   print("dingin")
+   
+
